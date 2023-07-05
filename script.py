2ed5dcf7a352585e503a5dbf8932d0f39da099e0
Megh <hirparamegh@gmail.com>
2023-07-05 16:31:38 -0400

    Uses VSB to check availability
    Moved from REM to VSB for checking availability. Updated the performance of the script and included error handling.

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-
 import time
+from datetime import datetime
 
 options = Options()
 options.add_experimental_option('detach', True)
@@ -13,78 +15,101 @@ options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=options)
 
-driver.get('https://wrem.sis.yorku.ca/Apps/WebObjects/REM.woa/wa/DirectAction/rem')
+rem = "https://wrem.sis.yorku.ca/Apps/WebObjects/REM.woa/wa/DirectAction/rem"
+vsb = 'https://schedulebuilder.yorku.ca/vsb/criteria.jsp?access=0&lang=en&tip=1&page=results&scratch=0&term=0&sort' \
+      '=none&filters=iiiiiiii&bbs=&ds=&cams=0_1_2_3_4_5_6_7_8&locs=any '
 
-# Enter username and password to sign in York Passport
+driver.get(vsb)
+
+# Enter the following information
 username = ""
 password = ""
+catalogue_number = ""
 
 driver.find_element(By.ID, 'mli').send_keys(username)
 driver.find_element(By.ID, 'password').send_keys(password)
 driver.find_element(By.NAME, 'dologin').click()
 
-time.sleep(5)
-driver.implicitly_wait(5)
 
-# Logged in successfully
+def add_course():
+    # Select Fall/Winter 24 from options, click Continue
+    select = Select(driver.find_element(By.NAME, '5.5.1.27.1.11.0'))
+    select.select_by_value('3')
+    driver.find_element(By.XPATH, '//input[@value="Continue"]').click()
 
-# Select Fall/Winter 24 from options, click Continue
-select = Select(driver.find_element(By.NAME, '5.5.1.27.1.11.0'))
-select.select_by_value('3')
-driver.find_element(By.NAME, '5.5.1.27.1.13').click()
-
-time.sleep(2)
-driver.implicitly_wait(2)
-
-# Start Loop
-
-flag = False
-
-while flag is False:
     # click Add a Course
-    driver.find_element(By.NAME, '5.1.27.1.23').click()
-    time.sleep(2)
-    driver.implicitly_wait(2)
-
-    # Enter the course catalogue number as a string
-    catalogue_number = ""
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//input[@title="Add a Course"]'))
+    ).click()
 
     # Enter catalogue number, click Add Course
-    driver.find_element(By.NAME, '5.1.27.7.7').send_keys(catalogue_number)
-    driver.find_element(By.NAME, '5.1.27.7.9').click()
-
-    time.sleep(2)
-    driver.implicitly_wait(2)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//input[@type="text"]'))
+    ).send_keys(catalogue_number)
+    driver.find_element(By.XPATH, '//input[@type="submit"]').click()
 
     # Are you sure? Yes
-    driver.find_element(By.NAME, '5.1.27.11.11').click()
-
-    time.sleep(2)
-    driver.implicitly_wait(2)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//input[@value="Yes"]'))
+    ).click()
 
     # 2 possibilities, either failed or succeeded
 
-    # check if failed
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'bodytext'))
+    )
     results_text = driver.find_elements(By.CLASS_NAME, 'bodytext')
-    success = "The course has been successfully added."
 
-    for x in results_text:
-        if x.text == success:
-            # Course added successfully, click continue and exit loop
-            driver.find_element(By.NAME, '5.1.27.23.9').click()
-            flag = True
-            break
+    for i in range(len(results_text)):
+        if results_text[i].text == "Result:":
+            # print Result
+            print(results_text[i].text + ' ' + results_text[i + 1].text + '\n')
 
-    # value did not change => course failed to be added
-    if flag is False:
+            if results_text[i + 1].text == "The course has been successfully added.":
+                # Course added successfully, click continue, return True
+                driver.find_element(By.XPATH, '//input[@value="Continue"]').click()
+                return True
 
-        # Failed, click continue, start loop again
-        driver.find_element(By.NAME, '5.1.27.27.11').click()
-        print("Course could now be added, trying again in 30 sec :)")
-        time.sleep(300)
-        driver.implicitly_wait(300)
+            else:
+                # Course could not be added, print the Reason, click continue, return False
+                print(results_text[i + 2].text + ' ' + results_text[i + 3].text)
+                driver.find_element(By.XPATH, '//input[@value="Continue"]').click()
+                return False
+
+
+print("Start Time =", datetime.now().strftime("%H:%M:%S"))
+available = False
+while available is False:
+    driver.get(vsb)
+
+    WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.ID, 'term_2023102119'))
+    ).click()
+    driver.find_element(By.ID, 'code_number').send_keys(catalogue_number)
+    driver.find_element(By.ID, 'addCourseButton').click()
+
+    time.sleep(1)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//span[text()="' + catalogue_number + '"]/../../span[text()]'))
+    )
+
+    element = driver.find_element(By.XPATH,
+                                  '//span[text()="' + catalogue_number + '"]/../../span[text()]')
+
+    if element.text == "Seats: Available":
+        print('seats available!!! Trying to add the course.')
+        driver.get(rem)
+        time.sleep(1)
+        available = add_course()
+        if available is False:
+            # Course could not be added because seats are reserved
+            print("Trying again in 15 minutes!")
+            print("Current Time =", datetime.now().strftime("%H:%M:%S"))
+            time.sleep(900)
 
     else:
-        print("Successfully added the course")
+        print('seats NOT available, trying again :)')
+        time.sleep(5)
 
+print("End Time =", datetime.now().strftime("%H:%M:%S"))
 driver.close()
